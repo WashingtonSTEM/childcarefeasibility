32214e38d16c7bb98e3ada21f4e7d76b20904ca7
Victor Alessandro Meza Lodato <victoraless@gmail.com>
2023-03-09 15:19:31 -0300

    feat: Title and TexBox components

---
+++ src/components/TextBox.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const TextBox = styled.div`
+  width: 100%;
+  background: #012846;
+  border-radius: 4px;
+  color: #F3F3F3;
+  padding: 20px 16px;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+`
+
+export default TextBox
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 30px;
+  color: #012846;
+`
+
+export default Title
