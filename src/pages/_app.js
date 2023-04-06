366525a2a16ab4e220ee8c1dd25d401fe2665e17
Ronald Castillo <ronaldcastillo@gmail.com>
2023-04-06 17:24:24 -0400

    Update step one and two

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
 import { IntlProvider } from 'react-intl'
+import { useRouter } from 'next/router'
 
 import messages from '@/data/intl'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  let language = 'en'
+
+  if (router.query?.language) {
+    language = router.query?.language
+  }
+
   return (
-    <IntlProvider messages={ messages['es'] } locale="es" defaultLocale="en">
+    <IntlProvider messages={ messages[language] ?? messages['en'] } locale={ language } defaultLocale="en">
       <Component {...pageProps} />
     </IntlProvider>
   )
