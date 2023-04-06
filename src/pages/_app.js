a866da8189d61dc01cd9c2fc76e1e1fd3f3e15cf
Ronald Castillo <ronaldcastillo@gmail.com>
2023-04-06 10:31:16 -0400

    Adding provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { IntlProvider } from 'react-intl'
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const messages = require('../data/intl/en.json');
+  return (
+    <IntlProvider messages={ messages } locale="en" defaultLocale="en">
+      <Component {...pageProps} />
+    </IntlProvider>
+  )
 }
